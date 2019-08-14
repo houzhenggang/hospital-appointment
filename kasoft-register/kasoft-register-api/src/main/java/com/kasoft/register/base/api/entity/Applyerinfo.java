fdc98f45f822a216c562a15a975ac9029df466b4
15565502588 <1083584170@qq.com>
2019-08-14 23:14:37 +0800

    修改

--- kasoft-register/kasoft-register-api/src/main/java/com/kasoft/register/base/api/entity/Applyerinfo.java
+++ kasoft-register/kasoft-register-api/src/main/java/com/kasoft/register/base/api/entity/Applyerinfo.java
@@ -1,5 +1,6 @@
 package com.kasoft.register.base.api.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
